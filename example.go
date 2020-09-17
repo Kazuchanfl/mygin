cf1aeaa3622a96506f237d03b3e78dc71e512cd9
高橋和晃 <kazuaki.flute@gmail.com>
2020-09-17 09:30:49 +0900

    write the string into the response body

--- example.go
+++ example.go
@@ -4,10 +4,8 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	router.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello, World!")
 	})
 	router.Run()
 }
