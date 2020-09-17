28a73cdc2a887fdb690dc489e3177fd946e96fe1
高橋和晃 <kazuaki.flute@gmail.com>
2020-09-17 09:36:15 +0900

    get params in the path

--- example.go
+++ example.go
@@ -1,11 +1,18 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
 	router := gin.Default()
 	router.GET("/", func(c *gin.Context) {
 		c.String(200, "Hello, World!")
+	})
+	router.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello, %s!", name)
 	})
 	router.Run()
 }
